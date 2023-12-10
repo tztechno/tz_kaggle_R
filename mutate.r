ff3c3314f2ae3001eaed9af1434ce0d462d18bff
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-10 18:15:50 +0900

    Update mutate.r

--- mutate.r
+++ mutate.r
@@ -1,14 +1,16 @@
-# create new columns
+#
 
-df <- df %>%
-  mutate(
-    new_column = existing_column * 2
-  )
 
+# mutate()を使用する場合
+bike_trips_2020 <- bike_trips_2020 %>%
+  mutate(ride_minutes = difftime(ended_at, started_at, units = "mins"))
+
+
+# 直接新しい列を追加する場合
+bike_trips_2020$ride_minutes <- difftime(bike_trips_2020$ended_at, bike_trips_2020$started_at, units = "mins")
 
 
 # add new column of no data
-
 df[[new_column_name]] <- NA
 
 
