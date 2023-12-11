aee5359bb20554474e09e4ee5ef912ff070f2062
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-11 17:19:11 +0900

    Update as_character.r

--- as_character.r
+++ as_character.r
@@ -4,3 +4,9 @@ covid_data_train[["Country_Region"]] <- as.character(covid_data_train[["Country_
 
 covid_data_train[["Date"]] <- as.Date(covid_data_train[["Date"]], format = "%Y-%m-%d")
 
+
+#### ifelse構文、np.whereに似ている
+covid_data_train[["Province_State"]] <- ifelse(covid_data_train[["Province_State"]] == "", covid_data_train[["Country_Region"]], covid_data_train[["Province_State"]])
+
+
+
