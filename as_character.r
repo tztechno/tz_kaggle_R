bef779da5bb71559284717cfc142712e43311397
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-11 17:29:53 +0900

    Update as_character.r

--- as_character.r
+++ as_character.r
@@ -1,5 +1,5 @@
 
-
+#### 属性変更
 covid_data_train[["Country_Region"]] <- as.character(covid_data_train[["Country_Region"]] )
 
 covid_data_train[["Date"]] <- as.Date(covid_data_train[["Date"]], format = "%Y-%m-%d")
@@ -8,5 +8,8 @@ covid_data_train[["Date"]] <- as.Date(covid_data_train[["Date"]], format = "%Y-%
 #### ifelse構文、np.whereに似ている
 covid_data_train[["Province_State"]] <- ifelse(covid_data_train[["Province_State"]] == "", covid_data_train[["Country_Region"]], covid_data_train[["Province_State"]])
 
+#### 列ごとのna
+colSums(is.na(covid_data_train)) 
 
-
+#### df全体でのna
+sum(is.na(covid_data_train))
