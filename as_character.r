dcb46abadbc1553419c5897a630e283cd54ee91f
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-11 17:11:17 +0900

    Update as_character.r

--- as_character.r
+++ as_character.r
@@ -1,1 +1,6 @@
+
+
 covid_data_train[["Country_Region"]] <- as.character(covid_data_train[["Country_Region"]] )
+
+covid_data_train[["Date"]] <- as.Date(covid_data_train[["Date"]], format = "%Y-%m-%d")
+
