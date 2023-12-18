d18c70c5c8fe9dea86039646964d102e5a6a9c67
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-18 16:05:02 +0900

    Update library.r

--- library.r
+++ library.r
@@ -4,11 +4,19 @@ library(geosphere)
 library(tidyverse)
 
 library(timeSeries)
-library(dplyr) 
+library(dplyr) #表形式のデータを処理
 library(caret)
 library(ggplot2)
 library(forecast)
 library(lubridate)
 library(ModelMetrics)
+library(sparklyr) #R言語からSparkを利用するためのパッケージ
 
 set.seed(123) 
+
+
+
+#Sparkは、大規模データ処理のためのプラットフォーム
+spark_install()
+sc <- spark_connect(master = "local")
+
