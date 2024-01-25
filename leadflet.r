2d14a91d068ec2b5e5b944cf4b7d927e12fb51cc
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-25 12:46:21 +0900

    Create leadflet.r

---
+++ leadflet.r
@@ -0,0 +1,17 @@
+if (!requireNamespace("leaflet.extras", quietly = TRUE)) {
+  install.packages("leaflet.extras")
+}
+
+if (!requireNamespace("htmlwidgets", quietly = TRUE)) {
+  install.packages("htmlwidgets")
+}
+
+if (!requireNamespace("IRdisplay", quietly = TRUE)) {
+  install.packages("IRdisplay")
+}
+
+library(ggplot2)
+library(leaflet)
+library(leaflet.extras)
+library(htmlwidgets)
+library(IRdisplay)
